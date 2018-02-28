6cbec05e36255dd04e1549ac8eb0c613a3957568
Aurimas <aurimas1rimkus@gmail.com>
2018-02-28 15:47:50 +0200

    Add method for deleting Users

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -54,5 +55,18 @@ class AdminController extends AbstractController
         return $this->render('admin/new_user.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/users/{id}/delete", name="admin_delete_user")
+     * @Method("POST")
+     */
+    public function deleteUser(User $user): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return $this->redirectToRoute("admin_show_users");
     }
 }
