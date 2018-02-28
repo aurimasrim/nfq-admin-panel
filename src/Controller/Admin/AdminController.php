6cb938dae4140f3a44efefdcd76153fc2e215405
Aurimas <aurimas1rimkus@gmail.com>
2018-02-28 14:58:17 +0200

    Create form of new User

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +20,26 @@ class AdminController extends AbstractController
     public function index(): Response
     {
         return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/users/new", name="admin_new_user")
+     */
+    public function newUser(Request $request): Response
+    {
+        $form = $this->createForm(UserType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($form->getData());
+            $em->flush();
+
+            return $this->redirectToRoute('admin_index');
+        }
+
+        return $this->render('admin/new_user.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
