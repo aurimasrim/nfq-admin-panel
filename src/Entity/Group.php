2338e5aead955881557d8a2930f308384ae8ef9a
Aurimas <aurimas1rimkus@gmail.com>
2018-03-01 12:12:27 +0200

    Add Group to User Many-To-Many relationship

--- src/Entity/Group.php
+++ src/Entity/Group.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,14 @@ class Group
      */
     private $name;
 
+    /**
+     * @var User[]|ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="App\Entity\User")
+     * @ORM\JoinTable(name="groups_users")
+     */
+    private $users;
+
     public function __construct(string $name)
     {
         $this->name = $name;
@@ -44,5 +54,21 @@ class Group
     public function setName(string $name): void
     {
         $this->name = $name;
+    }
+
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): void
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+        }
+    }
+    public function removeUser(User $user): void
+    {
+        $this->users->removeElement($user);
     }
 }
