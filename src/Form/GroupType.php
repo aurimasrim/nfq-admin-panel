8dc5510ed729588f75c4f9c637c84c60ed7fda6e
Aurimas <aurimas1rimkus@gmail.com>
2018-03-04 14:24:27 +0200

    Change style of buttons

--- src/Form/GroupType.php
+++ src/Form/GroupType.php
@@ -14,10 +14,7 @@ class GroupType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('name', TextType::class)
-            ->add('add', SubmitType::class)
-        ;
+        $builder->add('name', TextType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
